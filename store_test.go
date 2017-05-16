430d28d7e328e8bda7053040365030e2ca3258ca
Whatcott, Michael <mdwhatcott@gmail.com>
2017-05-16 16:26:38 -0600

    Refactor tests to xunit-style fixtures provided by gunit.

--- store_test.go
+++ store_test.go
@@ -1,46 +1,66 @@
 package videostore
 
-import "testing"
+import (
+	"testing"
 
-func TestSingleNewReleaseStatement(t *testing.T) {
-	customer := &Customer{"Fred", nil}
-	customer.Add(&Rental{&Movie{"The Cell", NewRelease}, 3})
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
 
-	want := "Rental Record for Fred\n\tThe Cell\t9\nYou owed 9\nYou earned 2 frequent renter points\n"
-	if got := customer.Statement(); got != want {
-		t.Errorf("Got: %s\nWant: %s", got, want)
-	}
+func TestCustomerFixture(t *testing.T) { gunit.Run(new(CustomerFixture), t) }
+
+type CustomerFixture struct {
+	*gunit.Fixture
+
+	customer *Customer
 }
 
-func TestDualReleaseStatement(t *testing.T) {
-	customer := &Customer{"Fred", nil}
-	customer.Add(&Rental{&Movie{"The Cell", NewRelease}, 3})
-	customer.Add(&Rental{&Movie{"The Tigger Movie", NewRelease}, 3})
-
-	want := "Rental Record for Fred\n\tThe Cell\t9\n\tThe Tigger Movie\t9\nYou owed 18\nYou earned 4 frequent renter points\n"
-	if got := customer.Statement(); got != want {
-		t.Errorf("Got: %s\nWant: %s", got, want)
-	}
+func (this *CustomerFixture) Setup() {
+	this.customer = &Customer{"Fred", nil}
 }
 
-func TestSingleChildrensStatement(t *testing.T) {
-	customer := &Customer{"Fred", nil}
-	customer.Add(&Rental{&Movie{"The Tigger Movie", ChildrensMovie}, 3})
+func (this *CustomerFixture) TestSingleNewReleaseStatement() {
+	this.customer.Add(&Rental{&Movie{"The Cell", NewRelease}, 3})
 
-	want := "Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n"
-	if got := customer.Statement(); got != want {
-		t.Errorf("Got: %s\nWant: %s", got, want)
-	}
+	this.assertStatement("Rental Record for Fred\n" +
+		"\tThe Cell\t9\n" +
+		"You owed 9\n" +
+		"You earned 2 frequent renter points\n")
 }
 
-func TestMultipleRegularStatement(t *testing.T) {
-	customer := &Customer{"Fred", nil}
-	customer.Add(&Rental{&Movie{"Plan 9 from Outer Space", RegularMovie}, 1})
-	customer.Add(&Rental{&Movie{"8 1/2", RegularMovie}, 2})
-	customer.Add(&Rental{&Movie{"Eraserhead", RegularMovie}, 3})
+func (this *CustomerFixture) TestDualReleaseStatement() {
+	this.customer.Add(&Rental{&Movie{"The Cell", NewRelease}, 3})
+	this.customer.Add(&Rental{&Movie{"The Tigger Movie", NewRelease}, 3})
 
-	want := "Rental Record for Fred\n\tPlan 9 from Outer Space\t2\n\t8 1/2\t2\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n"
-	if got := customer.Statement(); got != want {
-		t.Errorf("Got: %s\nWant: %s", got, want)
-	}
+	this.assertStatement("Rental Record for Fred\n" +
+		"\tThe Cell\t9\n" +
+		"\tThe Tigger Movie\t9\n" +
+		"You owed 18\n" +
+		"You earned 4 frequent renter points\n")
+}
+
+func (this *CustomerFixture) TestSingleChildrensStatement() {
+	this.customer.Add(&Rental{&Movie{"The Tigger Movie", ChildrensMovie}, 3})
+
+	this.assertStatement("Rental Record for Fred\n" +
+		"\tThe Tigger Movie\t1.5\n" +
+		"You owed 1.5\n" +
+		"You earned 1 frequent renter points\n")
+}
+
+func (this *CustomerFixture) TestMultipleRegularStatement() {
+	this.customer.Add(&Rental{&Movie{"Plan 9 from Outer Space", RegularMovie}, 1})
+	this.customer.Add(&Rental{&Movie{"8 1/2", RegularMovie}, 2})
+	this.customer.Add(&Rental{&Movie{"Eraserhead", RegularMovie}, 3})
+
+	this.assertStatement("Rental Record for Fred\n" +
+		"\tPlan 9 from Outer Space\t2\n" +
+		"\t8 1/2\t2\n" +
+		"\tEraserhead\t3.5\n" +
+		"You owed 7.5\n" +
+		"You earned 3 frequent renter points\n")
+}
+
+func (this *CustomerFixture) assertStatement(expected interface{}) {
+	this.So(this.customer.Statement(), should.Equal, expected)
 }
